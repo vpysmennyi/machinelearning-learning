50c30142be09d8923027a63ffdb268196855a5e6
Viacheslav Pysmennyi <vpysmennyi@gmail.com>
2021-04-03 15:09:15 +0300

    V1.0

--- course_gpt2/from_colab.py
+++ course_gpt2/from_colab.py
@@ -1,7 +1,7 @@
 
-from google.colab import drive
 import time
 import datetime
+import json
 
 import seaborn as sns
 import pandas as pd
@@ -24,11 +24,12 @@ import torch_xla.test.test_utils as test_utils
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data import DataLoader, Dataset, SequentialSampler, RandomSampler, random_split
 
-import torch_xla
-import torch_xla.core.xla_model as xm
-
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
 from transformers import AdamW, get_linear_schedule_with_warmup
+
+# setup colab
+in_colab = False
+
 
 class ArxivDataset(Dataset):
     def __init__(self, data_list, tokenizer, max_length=768):
@@ -60,24 +61,31 @@ def get_distributed_sampler(dataset, shuffle=True):
         shuffle=shuffle)
     return sampler
 
-def generate_sample(model, tokenizer):
-    print('Generating sample:')
+def print_train_stats(stats):
+    pd.set_option('precision', 2)
+    stat = pd.DataFrame(data=stats)
+
+    stat = stat.set_index('epoch')
+    xm.master_print(stat)
+
+def generate_sample(model, tokenizer, top_k = 50, max_l = 200, top_p = 0.92, num_seq=1):
+    xm.master_print('Generating sample:')
     # moving to cpu for generation
     model.to('cpu')
     sample_output = model.generate(bos_token_id=random.randint(1, 30000),
                                    do_sample=True,
-                                   top_k=50,
-                                   max_length=200,
-                                   top_p=0.95,
-                                   num_return_sequences=1)
+                                   top_k=top_k,
+                                   max_length=max_l,
+                                   top_p=top_p,
+                                   num_return_sequences=num_seq)
     for i, sample in enumerate(sample_output):
-        print(f'{i} : {tokenizer.decode(sample, skip_special_tokens=True)}')
+        xm.master_print(f'{i} : {tokenizer.decode(sample, skip_special_tokens=True)}')
 
 
 def _run(model, tokenizer, dataset, tpu_dev):
-    batch_size = 2
+    batch_size = config['train_batch_size']
 
-    train_size = int(len(dataset) * 0.9)
+    train_size = int(len(dataset) * config['train_size_percent']/100)
     val_size = len(dataset) - train_size
 
     train_ds, val_ds = random_split(dataset, [train_size, val_size])
@@ -90,24 +98,24 @@ def _run(model, tokenizer, dataset, tpu_dev):
                         batch_size=batch_size)
 
     model.to(tpu_dev)
+    devices = xm.get_xla_supported_devices(max_devices=config['num_tpu_cores'])
+    model = dp.DataParallel(model, device_ids=devices)
 
-    epochs = 5
-    learning_rate = 5e-4 * xm.xrt_world_size()
-    warmup_steps = 1e2
-    epsilon = 1e-8
+    epochs = config['num_epochs']
+    learning_rate = config['lr'] * xm.xrt_world_size()
+    warmup_steps = config['warmup_steps']
+    epsilon = config['epsilon']
 
-    # this produces sample output every 100 steps
-    sample_every = 100
-
-    opt = AdamW(model.parameters(), lr=learning_rate)
+    opt = AdamW(model.parameters(), lr=learning_rate, eps=epsilon)
 
     total_steps = int(len(train_ds )/ batch_size / xm.xrt_world_size() * epochs)
 
     scheduler = get_linear_schedule_with_warmup(opt, num_warmup_steps=warmup_steps, num_training_steps=total_steps)
 
-    train_loss = 0
     t0 = time.time()
     xm.master_print(f'num_train_steps = {total_steps}, world_size={xm.xrt_world_size()}')
+
+    train_stats = []
 
     for epoch in range(epochs):
 
@@ -115,7 +123,8 @@ def _run(model, tokenizer, dataset, tpu_dev):
         p_val_dl = pl.ParallelLoader(val_dl, [tpu_dev])
 
         t0_epoch = time.time()
-        epoch_loss, val_epoch_loss = [], []
+        val_epoch_loss = 0
+        epoch_loss = 0
 
         model.train()
         for step, batch in enumerate(p_train_dl.per_device_loader(tpu_dev)):
@@ -132,14 +141,19 @@ def _run(model, tokenizer, dataset, tpu_dev):
                            token_type_ids=None)
 
             loss = output[0]
-            epoch_loss.append(loss.detach().cpu())
-            print(f'Step: {step}')
-            if step % sample_every == 0 and step > 0:
-                model.eval()
-                generate_sample(model, tokenizer)
-                # moving back to tpu to proceed with training
-                model.to(tpu_dev)
-                model.train()
+            epoch_loss += loss.item()
+
+            if step % 10 ==0:
+                xm.master_print(f'Step: {step}')
+
+            if config['mid_sample_enable']:
+                if step % config['sample_every_steps'] == 0 and step > 0:
+                    model.eval()
+
+                    generate_sample(model, tokenizer, config['decode_top_k'],config['decode_max_length'], config['decode_top_p'])
+                    #moving back to tpu to proceed with training
+                    model.to(tpu_dev)
+                    model.train()
 
             loss.backward()
 
@@ -147,15 +161,20 @@ def _run(model, tokenizer, dataset, tpu_dev):
             xm.optimizer_step(opt)
             scheduler.step()
 
+        train_time = format_time(time.time() - t0_epoch)
+        epoch_loss = epoch_loss /len(train_dl)
+
         if epoch_loss:
-            print \
-                (f'TRAIN | Epoch {epoch} : Loss = {torch.stack(epoch_loss).detach().cpu().mean()}. Spent: {format_time(time.time() - t0_epoch)}')
+            xm.master_print(f'TRAIN | Epoch {epoch + 1} : Loss = {epoch_loss}. Elapsed: {train_time}')
         else:
             print('Epoch loss is empty')
 
         model.eval()
-        for batch in p_val_dl.per_device_loader(tpu_dev):
 
+        val_t0 = time.time()
+
+        for val_step, batch in enumerate(p_val_dl.per_device_loader(tpu_dev)):
+            xm.master_print(f'Val step {val_step}')
             b_input_ids = batch[0].to(tpu_dev)
             b_labels = batch[0].to(tpu_dev)
             b_att_mask = batch[1].to(tpu_dev)
@@ -166,32 +185,77 @@ def _run(model, tokenizer, dataset, tpu_dev):
                            token_type_ids=None)
 
             loss = output[0]
-            val_epoch_loss.append(loss)
+            val_epoch_loss += loss.item()
+
+        val_time = format_time(time.time() - val_t0)
+        val_epoch_loss = val_epoch_loss /len(val_dl)
 
         if val_epoch_loss:
-            print(f'VAL | Epoch {epoch} : Loss = {torch.stack(val_epoch_loss).detach().cpu().mean()}')
+            xm.master_print(f'VAL | Epoch {epoch + 1} : Loss = {val_epoch_loss}. Elapsed: {val_time}')
         else:
             print('Empty val_epoch_loss')
 
-    print(f'Total elapsed: {format_time(time.time() - t0)}')
+        # Record all statistics from this epoch.
+        train_stats.append(
+            {
+                'epoch': epoch + 1,
+                'Training Loss': epoch_loss,
+                'Valid. Loss': val_epoch_loss,
+                'Training Time': train_time,
+                'Validation Time': val_time
+            }
+        )
+
+        xm.master_print('saving the model')
+        model_to_save = model.module if hasattr(model,
+                                                'module') else model  # Take care of distributed/parallel training
+        model_to_save.save_pretrained(config['save_dir'])
+        tokenizer.save_pretrained(config['save_dir'])
+        xm.master_print('saved')
+    xm.master_print(f'Total elapsed: {format_time(time.time() - t0)}')
+    print_train_stats(train_stats)
+
+    model.to('cpu')
+
+    # final result
+    xm.master_print('Testing the model')
+    generate_sample(model, tokenizer, num_seq=config['decode_num_test_samples'])
 
 def _mp_fn(rank, flags):
     torch.set_default_tensor_type('torch.FloatTensor')
+
     # initializing TPU device
     device = xm.xla_device()
     _run(mdl ,tokenizer ,dataset ,device)
 
-drive.mount('/content/drive', force_remount=True)
 nltk.download('punkt')
-df = pd.read_json('drive/MyDrive/ArxivDS/arxiv-abstracts.json', lines=True, nrows=1000)
 
-abstracts = df.abstract.copy()
+# reading config
+conf_file = './config.json'
+
+if in_colab:
+    from google.colab import drive
+    drive.mount('/content/drive')
+    conf_file = 'drive/MyDrive/ArxivDS/colab_config.json'
+
+with open(conf_file) as f:
+    config = json.load(f)
+
+print('Execution configuration:')
+for c in config:
+    print(f'{c}' + ' '*(30-len(c)) + f'{config[c]}')
+
+#reading dataset
+df = pd.read_json(config['datafile'], lines=True, nrows=2000)
+abstracts = df.abstract
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2', bos_token='<BOS>', eos_token='<EOS>', pad_token='<PAD>')
 gpt2_config = GPT2Config.from_pretrained('gpt2', output_hidden_states=False)
 mdl = GPT2LMHeadModel.from_pretrained('gpt2', config=gpt2_config)
 mdl.resize_token_embeddings(len(tokenizer))
-dataset = ArxivDataset(abstracts, tokenizer, max_length=512)
-FLAG S ={}
+dataset = ArxivDataset(abstracts, tokenizer, max_length=config['encoding_max_length'])
+FLAGS ={}
 
-xmp.spawn(_mp_fn, args=(FLAGS,), nprocs=1, start_method='fork')
+xmp.spawn(_mp_fn, args=(FLAGS,), nprocs=config['num_tpu_cores'], start_method='fork')
+
+#export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
