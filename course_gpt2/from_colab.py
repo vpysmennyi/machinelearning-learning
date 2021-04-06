e6ed701fbd7746c219f9fa7ab3d84024cc912bc4
Viacheslav Pysmennyi <vpysmennyi@gmail.com>
2021-04-06 21:02:57 +0300

    V3

--- course_gpt2/from_colab.py
+++ course_gpt2/from_colab.py
@@ -1,4 +1,3 @@
-
 import time
 import datetime
 import json
@@ -27,8 +26,8 @@ from torch.utils.data import DataLoader, Dataset, SequentialSampler, RandomSampl
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
 from transformers import AdamW, get_linear_schedule_with_warmup
 
-# setup colab
-in_colab = False
+# set to True when running in google colab
+in_colab = True
 
 
 class ArxivDataset(Dataset):
@@ -50,191 +49,215 @@ class ArxivDataset(Dataset):
         return self.input_ids[idx], self.attention_msk[idx]
 
 
-def format_time(elapsed):
-    return str(datetime.timedelta(seconds=int(round((elapsed)))))
+class ArxivAbstractGen():
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
 
-def get_distributed_sampler(dataset, shuffle=True):
-    sampler = DistributedSampler(
-        dataset,
-        num_replicas=xm.xrt_world_size(),
-        rank=xm.get_ordinal(),
-        shuffle=shuffle)
-    return sampler
+        self.dataset = ArxivDataset(abstracts, tokenizer, max_length=config['encoding_max_length'])
 
-def print_train_stats(stats):
-    pd.set_option('precision', 2)
-    stat = pd.DataFrame(data=stats)
+        self.epochs = config['num_epochs']
+        self.learning_rate = config['lr'] * xm.xrt_world_size()
+        self.warmup_steps = config['warmup_steps']
+        self.epsilon = config['epsilon']
+        self.batch_size = config['train_batch_size']
 
-    stat = stat.set_index('epoch')
-    xm.master_print(stat)
+        self.train_ds_len = 0
 
-def generate_sample(model, tokenizer, top_k = 50, max_l = 200, top_p = 0.92, num_seq=1):
-    xm.master_print('Generating sample:')
-    # moving to cpu for generation
-    model.to('cpu')
-    sample_output = model.generate(bos_token_id=random.randint(1, 30000),
-                                   do_sample=True,
-                                   top_k=top_k,
-                                   max_length=max_l,
-                                   top_p=top_p,
-                                   num_return_sequences=num_seq)
-    for i, sample in enumerate(sample_output):
-        xm.master_print(f'{i} : {tokenizer.decode(sample, skip_special_tokens=True)}')
+    def get_model(self):
+        return self.model
 
+    def prepare_data_loader(self, ds):
+        def _get_distributed_sampler(sliced_ds, shuffle=True):
+            sampler = DistributedSampler(
+                sliced_ds,
+                num_replicas=xm.xrt_world_size(),
+                rank=xm.get_ordinal(),
+                shuffle=shuffle)
+            return sampler
 
-def _run(model, tokenizer, dataset, tpu_dev):
-    batch_size = config['train_batch_size']
+        train_size = int(len(ds) * config['train_size_percent'] / 100)
+        val_size = len(ds) - train_size
 
-    train_size = int(len(dataset) * config['train_size_percent']/100)
-    val_size = len(dataset) - train_size
+        train_ds, val_ds = random_split(ds, [train_size, val_size])
 
-    train_ds, val_ds = random_split(dataset, [train_size, val_size])
+        self.train_ds_len = len(train_ds)
 
-    train_dl = DataLoader(train_ds,
-                          sampler=get_distributed_sampler(train_ds ,shuffle=True),
-                          batch_size=batch_size)
-    val_dl = DataLoader(val_ds,
-                        sampler=get_distributed_sampler(val_ds ,shuffle=False),
-                        batch_size=batch_size)
+        train_dl = DataLoader(train_ds,
+                              sampler=_get_distributed_sampler(train_ds, shuffle=True),
+                              batch_size=self.batch_size)
+        val_dl = DataLoader(val_ds,
+                            sampler=_get_distributed_sampler(val_ds, shuffle=False),
+                            batch_size=self.batch_size)
 
-    model.to(tpu_dev)
-    devices = xm.get_xla_supported_devices(max_devices=config['num_tpu_cores'])
-    model = dp.DataParallel(model, device_ids=devices)
+        return train_dl, val_dl
 
-    epochs = config['num_epochs']
-    learning_rate = config['lr'] * xm.xrt_world_size()
-    warmup_steps = config['warmup_steps']
-    epsilon = config['epsilon']
-
-    opt = AdamW(model.parameters(), lr=learning_rate, eps=epsilon)
-
-    total_steps = int(len(train_ds )/ batch_size / xm.xrt_world_size() * epochs)
-
-    scheduler = get_linear_schedule_with_warmup(opt, num_warmup_steps=warmup_steps, num_training_steps=total_steps)
-
-    t0 = time.time()
-    xm.master_print(f'num_train_steps = {total_steps}, world_size={xm.xrt_world_size()}')
-
-    train_stats = []
-
-    for epoch in range(epochs):
-
-        p_train_dl = pl.ParallelLoader(train_dl, [tpu_dev])
-        p_val_dl = pl.ParallelLoader(val_dl, [tpu_dev])
-
-        t0_epoch = time.time()
-        val_epoch_loss = 0
-        epoch_loss = 0
-
-        model.train()
-        for step, batch in enumerate(p_train_dl.per_device_loader(tpu_dev)):
-
-            opt.zero_grad()
-
-            b_input_ids = batch[0].to(tpu_dev)
-            b_labels = batch[0].to(tpu_dev)
-            b_att_mask = batch[1].to(tpu_dev)
-
-            output = model(b_input_ids,
-                           labels=b_labels,
-                           attention_mask=b_att_mask,
-                           token_type_ids=None)
-
-            loss = output[0]
-            epoch_loss += loss.item()
-
-            if step % 10 ==0:
-                xm.master_print(f'Step: {step}')
-
-            if config['mid_sample_enable']:
-                if step % config['sample_every_steps'] == 0 and step > 0:
-                    model.eval()
-
-                    generate_sample(model, tokenizer, config['decode_top_k'],config['decode_max_length'], config['decode_top_p'])
-                    #moving back to tpu to proceed with training
-                    model.to(tpu_dev)
-                    model.train()
-
-            loss.backward()
-
-            # need to use this for parallelism
-            xm.optimizer_step(opt)
-            scheduler.step()
-
-        train_time = format_time(time.time() - t0_epoch)
-        epoch_loss = epoch_loss /len(train_dl)
-
-        if epoch_loss:
-            xm.master_print(f'TRAIN | Epoch {epoch + 1} : Loss = {epoch_loss}. Elapsed: {train_time}')
-        else:
-            print('Epoch loss is empty')
-
-        model.eval()
-
-        val_t0 = time.time()
-
-        for val_step, batch in enumerate(p_val_dl.per_device_loader(tpu_dev)):
-            xm.master_print(f'Val step {val_step}')
-            b_input_ids = batch[0].to(tpu_dev)
-            b_labels = batch[0].to(tpu_dev)
-            b_att_mask = batch[1].to(tpu_dev)
-
-            output = model(b_input_ids,
-                           labels=b_labels,
-                           attention_mask=b_att_mask,
-                           token_type_ids=None)
-
-            loss = output[0]
-            val_epoch_loss += loss.item()
-
-        val_time = format_time(time.time() - val_t0)
-        val_epoch_loss = val_epoch_loss /len(val_dl)
-
-        if val_epoch_loss:
-            xm.master_print(f'VAL | Epoch {epoch + 1} : Loss = {val_epoch_loss}. Elapsed: {val_time}')
-        else:
-            print('Empty val_epoch_loss')
-
-        # Record all statistics from this epoch.
-        train_stats.append(
-            {
-                'epoch': epoch + 1,
-                'Training Loss': epoch_loss,
-                'Valid. Loss': val_epoch_loss,
-                'Training Time': train_time,
-                'Validation Time': val_time
-            }
-        )
-
+    def model_save(self):
         xm.master_print('saving the model')
-        model_to_save = model.module if hasattr(model,
-                                                'module') else model  # Take care of distributed/parallel training
+        model_to_save = self.model.module if hasattr(self.model,
+                                                     'module') else self.model  # Take care of distributed/parallel training
         model_to_save.save_pretrained(config['save_dir'])
         tokenizer.save_pretrained(config['save_dir'])
         xm.master_print('saved')
-    xm.master_print(f'Total elapsed: {format_time(time.time() - t0)}')
-    print_train_stats(train_stats)
 
-    model.to('cpu')
+    def print_train_stats(self, stats):
+        pd.set_option('precision', 2)
+        stat = pd.DataFrame(data=stats)
 
-    # final result
-    xm.master_print('Testing the model')
-    generate_sample(model, tokenizer, num_seq=config['decode_num_test_samples'])
+        stat = stat.set_index('epoch')
+        xm.master_print(stat)
 
-def _mp_fn(rank, flags):
+    def generate_sample(self, top_k=50, max_l=200, top_p=0.92, num_seq=1):
+        xm.master_print('Generating sample:')
+        # moving to cpu for generation
+        self.model.to('cpu')
+        sample_output = self.model.generate(bos_token_id=random.randint(1, 30000),
+                                            do_sample=True,
+                                            top_k=top_k,
+                                            max_length=max_l,
+                                            top_p=top_p,
+                                            num_return_sequences=num_seq)
+        for i, sample in enumerate(sample_output):
+            xm.master_print(f'{i} : {self.tokenizer.decode(sample, skip_special_tokens=True)}')
+
+    def run_training(self, tpu_dev):
+
+        # get data loader sets
+        train_dl, val_dl = self.prepare_data_loader(self.dataset)
+
+        self.model.to(tpu_dev)
+
+        opt = AdamW(self.model.parameters(), lr=self.learning_rate, eps=self.epsilon)
+
+        total_steps = int(self.train_ds_len / self.batch_size / xm.xrt_world_size() * self.epochs)
+
+        scheduler = get_linear_schedule_with_warmup(opt,
+                                                    num_warmup_steps=self.warmup_steps,
+                                                    num_training_steps=total_steps)
+
+        t0 = time.time()
+        xm.master_print(f'num_train_steps = {total_steps}, TPU cores={xm.xrt_world_size()}')
+
+        train_stats = []
+
+        for epoch in range(self.epochs):
+            t0_epoch = time.time()
+
+            p_train_dl = pl.ParallelLoader(train_dl, [tpu_dev])
+            p_val_dl = pl.ParallelLoader(val_dl, [tpu_dev])
+
+            # devices = xm.get_xla_supported_devices(max_devices=config['num_tpu_cores'])
+            # model = dp.DataParallel(self.model, device_ids=devices)
+
+            val_epoch_loss = 0
+            epoch_loss = 0
+
+            ############ TRAINING
+            self.model.train()
+            for step, batch in enumerate(p_train_dl.per_device_loader(tpu_dev)):
+
+                opt.zero_grad()
+
+                b_input_ids = batch[0].to(tpu_dev)
+                b_labels = batch[0].to(tpu_dev)
+                b_att_mask = batch[1].to(tpu_dev)
+
+                output = self.model(b_input_ids,
+                                    labels=b_labels,
+                                    attention_mask=b_att_mask,
+                                    token_type_ids=None)
+
+                loss = output[0]
+                epoch_loss += loss.item()
+
+                if step % 10 == 0:
+                    xm.master_print(f'Step: {step}')
+
+                '''if config['mid_sample_enable']:
+                    if step % config['sample_every_steps'] == 0 and step > 0:
+                        model.eval()
+
+                        generate_sample(model, tokenizer, config['decode_top_k'], config['decode_max_length'],
+                                        config['decode_top_p'])
+                        # moving back to tpu to proceed with training
+                        model.to(tpu_dev)
+                        model.train()'''
+
+                loss.backward()
+
+                # need to use this for parallelism
+                xm.optimizer_step(opt)
+
+                scheduler.step()
+
+            train_time = format_time(time.time() - t0_epoch)
+            epoch_loss = epoch_loss / len(train_dl)
+
+            if epoch_loss:
+                xm.master_print(f'TRAIN | Epoch {epoch + 1} : Loss = {epoch_loss}. Elapsed: {train_time}')
+            else:
+                print('Epoch loss is empty')
+
+            ############ VALIDATION
+            self.model.eval()
+
+            val_t0 = time.time()
+
+            for val_step, batch in enumerate(p_val_dl.per_device_loader(tpu_dev)):
+                xm.master_print(f'Val step {val_step}')
+                b_input_ids = batch[0].to(tpu_dev)
+                b_labels = batch[0].to(tpu_dev)
+                b_att_mask = batch[1].to(tpu_dev)
+
+                output = self.model(b_input_ids,
+                                    labels=b_labels,
+                                    attention_mask=b_att_mask,
+                                    token_type_ids=None)
+
+                loss = output[0]
+                val_epoch_loss += loss.item()
+
+            val_time = format_time(time.time() - val_t0)
+            val_epoch_loss = val_epoch_loss / len(val_dl)
+
+            if val_epoch_loss:
+                xm.master_print(f'VAL | Epoch {epoch + 1} : Loss = {val_epoch_loss}. Elapsed: {val_time}')
+            else:
+                print('Empty val_epoch_loss')
+
+            # Record all statistics from this epoch.
+            train_stats.append(
+                {
+                    'epoch': epoch + 1,
+                    'Training Loss': epoch_loss,
+                    'Valid. Loss': val_epoch_loss,
+                    'Training Time': train_time,
+                    'Validation Time': val_time
+                }
+            )
+
+        xm.master_print(f'Total elapsed: {format_time(time.time() - t0)}')
+        self.print_train_stats(train_stats)
+
+
+def format_time(elapsed):
+    return str(datetime.timedelta(seconds=int(round(elapsed))))
+
+
+def _mp_fn(rank, flags, trainer_obj):
     torch.set_default_tensor_type('torch.FloatTensor')
 
     # initializing TPU device
     device = xm.xla_device()
-    _run(mdl ,tokenizer ,dataset ,device)
+    trainer_obj.run_training(device)
 
-nltk.download('punkt')
 
 # reading config
 conf_file = './config.json'
 
 if in_colab:
     from google.colab import drive
+
     drive.mount('/content/drive')
     conf_file = 'drive/MyDrive/ArxivDS/colab_config.json'
 
@@ -243,19 +266,29 @@ with open(conf_file) as f:
 
 print('Execution configuration:')
 for c in config:
-    print(f'{c}' + ' '*(30-len(c)) + f'{config[c]}')
+    print(f'{c}' + ' ' * (30 - len(c)) + f'{config[c]}')
 
-#reading dataset
+# reading dataset
 df = pd.read_json(config['datafile'], lines=True, nrows=2000)
 abstracts = df.abstract
 
+# loading GPT2
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2', bos_token='<BOS>', eos_token='<EOS>', pad_token='<PAD>')
 gpt2_config = GPT2Config.from_pretrained('gpt2', output_hidden_states=False)
 mdl = GPT2LMHeadModel.from_pretrained('gpt2', config=gpt2_config)
 mdl.resize_token_embeddings(len(tokenizer))
-dataset = ArxivDataset(abstracts, tokenizer, max_length=config['encoding_max_length'])
-FLAGS ={}
 
-xmp.spawn(_mp_fn, args=(FLAGS,), nprocs=config['num_tpu_cores'], start_method='fork')
+trainer = ArxivAbstractGen(mdl, tokenizer)
 
-#export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
+xmp.spawn(_mp_fn, args=(config, trainer), nprocs=config['num_tpu_cores'], start_method='fork')
+
+# generating samples with a tuned model
+trainer.generate_sample(top_k=config['decode_top_k'],
+                        max_l=config['decode_max_length'],
+                        top_p=config['decode_top_p'],
+                        num_seq=config['decode_num_test_samples'])
+
+# saving tuned model
+trainer.model_save()
+
+# export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
